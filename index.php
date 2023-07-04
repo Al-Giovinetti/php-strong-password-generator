6d686735abfa29b9f89a7333bcf79c9dc1e20476
Al-Giovinetti <giovinettialessio@gmail.com>
2023-07-04 14:55:42 +0200

    Creato file e form

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <h1>Password Generator 2.0</h1>
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+    </form>
+    
+    
+</body>
+</html>
